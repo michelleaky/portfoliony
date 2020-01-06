5ce8e87c1dbae879f102ba05cf52b474d92ff704
Chellemussemissedoffedelle Young <chellemussemissedoffedelleyoung@chellemussemissedoffedelles-macbook-air.home>
2020-01-06 19:38:57 +0100

    favicon sat ind, om mig billeder sat ind og javascript mht skiftende billeder af mig

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ function sidenVises() {
     clickMenu();
 
     document.querySelector("#menuknap").addEventListener("click", clickMenu);
+
+    document.querySelector("#michelle").addEventListener("mouseover", mouseOverBillede)
+
+    document.querySelector("#cafemichelle").addEventListener("mouseover", mouseOverBillede2)
 }
 
 function clickMenu() {
@@ -24,4 +28,44 @@ function clickFjern() {
 
     document.querySelector("#menuknap").removeEventListener("click", clickFjern);
     document.querySelector("#menuknap").addEventListener("click", clickMenu);
+}
+
+function mouseOverBillede() {
+    console.log("mouseOverBillede");
+
+    document.querySelector("#michelle").classList.add("hidden");
+    document.querySelector("#blomster").classList.remove("hidden");
+
+
+    document.querySelector("#blomster").classList.add("fade");
+    document.querySelector("#blomster").addEventListener("mouseout", mouseOutBillede);
+}
+
+function mouseOutBillede() {
+    console.log("mouseOutBillede");
+
+    document.querySelector("#blomster").classList.add("hidden");
+
+    document.querySelector("#michelle").classList.add("fade");
+    document.querySelector("#michelle").classList.remove("hidden");
+}
+
+function mouseOverBillede2() {
+    console.log("mouseOverBillede2");
+
+    document.querySelector("#cafemichelle").classList.add("hidden");
+    document.querySelector("#lillemichelle").classList.remove("hidden");
+
+
+    document.querySelector("#lillemichelle").classList.add("fade");
+    document.querySelector("#lillemichelle").addEventListener("mouseout", mouseOutBillede2);
+}
+
+function mouseOutBillede2() {
+    console.log("mouseOutBillede2");
+
+    document.querySelector("#lillemichelle").classList.add("hidden");
+
+    document.querySelector("#cafemichelle").classList.add("fade");
+    document.querySelector("#cafemichelle").classList.remove("hidden");
 }
